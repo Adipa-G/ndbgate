8a12b2c3a280d40958537f0c50db4ec3904fc57f
adipa.achintha@gmail.com <adipa.achintha@gmail.com@f24f5acc-74fa-dd37-0642-d3e278ae9704>
2014-07-24 12:37:08 +0000

    Multiple database support - r72 of dbgate

---
+++ trunk/DbGate/src-tests/Support/Persistant/MultiDb/MultiDbEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbGate.src_tests.Support.Persistant.MultiDb
+{
+    [TableInfo("multi_db_test_root")]
+    public class MultiDbEntity : DefaultEntity
+    {
+        [ColumnInfo((ColumnType.Integer), Key = true)]
+        public virtual int IdCol { get; set; }
+
+        [ColumnInfo(ColumnType.Varchar)]
+        public virtual string Name { get; set; }
+    }
+}
